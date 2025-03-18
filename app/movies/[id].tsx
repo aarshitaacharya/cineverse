06398ffbd1b606bcc72ee7af24d35c8c587a24cf
Aarshita Acharya <aarshitaacharya@Aarshitas-MacBook-Pro.local>
2025-03-17 20:17:53 -0500

    done till routing tabs

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text>Moviee Detailss </Text>
+    </View>
+  )
+}
+
+export default MovieDetails
